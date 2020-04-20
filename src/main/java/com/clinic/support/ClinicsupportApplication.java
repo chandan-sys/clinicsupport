e7c93ce8b01e9f7736fb311bda6a30dd125c4ab3
chandan <chandan.nayak777@gmail.com>
2020-04-20 11:19:32 +0530

    first commit

---
+++ src/main/java/com/clinic/support/ClinicsupportApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicsupportApplication.class, args);
+	}
+
+}
